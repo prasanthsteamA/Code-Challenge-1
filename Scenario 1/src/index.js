2dde29d14c61da0c86f198fa673002b19eae4f4b
ragul-steam-A <ragul@steam-1.com>
2024-01-24 16:45:28 +0530

    completed

--- Scenario 1/src/index.js
+++ Scenario 1/src/index.js
@@ -5,10 +5,26 @@
 // If nth is not positive, return -1
 // Keep in mind that 42 = 00042. This means that findDigit(42, 5) would return 0
 
-
 // Write the function to find the findDigit obtained.
-var findDigit = function(num, nth){
-     //your code here
+var findDigit = function (num, nth) {
+  //your code here
+  if(num<0)
+  {
+    num=-num;
   }
+  if(nth<=0)
+  {
+    return -1;
+  }
+  let cnt=0;
+  let digit=0;
+  while(num>0 && cnt<nth)
+  {
+    digit=num%10;
+    num=Math.floor(num/10);
+    cnt++;
+  }
+  return digit;
+};
 
 //Use SpecRunner to check the Test Cases.
--- Scenario 2/src/index.js
+++ Scenario 2/src/index.js
@@ -10,7 +10,35 @@
 
 
 // Write the function to find the getGrade obtained.
+//grade checker
+
+function gradeChecker(grade)
+{
+  if(grade>=90)
+  {
+    return 'A';
+  }
+  else if(grade>=80 && grade<90)
+  {
+    return 'B';
+  }
+  else if(grade>=70 && grade<80)
+  {
+    return 'C';
+  }
+  else if(grade>=60 && grade<70)
+  {
+    return 'D';
+  }
+  else
+  {
+    return 'F';
+  }
+}
+
+
 function getGrade (s1, s2, s3) {
    //your code here
+   return gradeChecker(s1)+' '+gradeChecker(s2)+' '+gradeChecker(s3);
   }
 //Use SpecRunner to check the Test Cases.
